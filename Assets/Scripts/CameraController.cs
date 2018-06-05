eb5e2d8ebe43a916f95738fe923e2af488691918
Sasha Usoskin <sashausoskin@live.com>
2018-06-05 12:51:47 +0300

    Liikkuminen
    Ensimmäinen versio raketista.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Rocket;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 RocketLocation = new Vector3(Rocket.transform.position.x, Rocket.transform.position.y, Rocket.transform.position.z - 10);
+        gameObject.transform.localPosition = (RocketLocation);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    bool Move;
+    private Rigidbody2D rb2d;
+    public float thrust;
+    public float rotationspeed;
+    public GameObject Fire;
+
+   
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        Fire.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Vertical"))
+        {
+           
+            {
+                rb2d.AddForce(transform.up * thrust);
+                Fire.SetActive(true);
+            }
+        }
+
+        if (Input.GetButton("Horizontal")){
+            rb2d.rotation = rb2d.rotation + rotationspeed * Input.GetAxis("Horizontal") * -1;
+        }
+
+        if (Input.GetButtonUp("Vertical"))
+        {
+            Fire.SetActive(false);
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
