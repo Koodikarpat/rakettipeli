14d47ad3ee32216bd5f3d0f9155da3dfbe382559
Unknown <anders.nikula@gmail.com>
2018-06-05 13:48:46 +0300

    Ampuminen

---
+++ Assets/Scripts/AmmusInstantiate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmusInstantiate : MonoBehaviour {
+
+    public Rigidbody AmmusPrefab;
+    public Transform barrelEnd;
+    Vector2 ammuksenSuunta;
+    public float ammuksenNopeus;
+
+	void Update () {
+        ammuksenSuunta = barrelEnd.position - transform.position;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody AmmusInstance;
+            AmmusInstance = Instantiate (AmmusPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            AmmusInstance.AddForce(ammuksenSuunta * ammuksenNopeus);
+            Debug.Log(barrelEnd.forward);
+        }
+	}
+}
