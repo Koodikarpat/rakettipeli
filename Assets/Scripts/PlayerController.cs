1fb486fd58ef37c723e471727349c472ee5c819c
Unknown <sashausoskin@live.com>
2018-06-07 10:10:18 +0300

    GameOver WIP 2
    GameOver-näyttöön on lisätty nappeja, jotka eivät vielä toimi. Se myös äkkiä renderöi animaation viimeisen framen ennen kuin se alkaa "soittamaan" animaatiota ensimmäisestä framesta.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,23 @@ public class PlayerController : MonoBehaviour {
 
     bool Move;
     private Rigidbody2D rb2d;
-    public float thrust;
-    public float rotationspeed;
+    public float thrust; //Raketin nopeus.
+    public float rotationspeed; //Raketin kääntönopeus.
     //public GameObject Fire;
-    public float level;
-    public GameObject[] Level1Barrels;
-    public GameObject[] Level2Barrels;
-    public GameObject ExplosionSprite;
-    public float InvincibilityTime;
-    public float InvincibilitySpeed;
-    private bool IsInvincible;
+    public float level; //Pelaajan leveli. Nousee, kun nappaa powerupin.
+    public GameObject[] Level1Barrels; //Barrelit, joita käytetään levelissä 1.
+    public GameObject[] Level2Barrels; //Barrelit, joita käytetään levelissä 2.
+    public GameObject ExplosionSprite; //Sprite, joka näkyy pelaajan kuoltaessa.
+    public float InvincibilityTime; //Aika, jolloin pelaaja on näkymätön damagen ottamisen jälkeen.
+    public float InvincibilitySpeed; //Nopeus, jolla raketti "vilkkuu" oltaessaan näkymättömässä tilanteessa.
+    private bool IsInvincible; //Jos pelaaja on näkymätön, viholliset eivät voi tehdä vahinkoa.
+    public Canvas Canvas;
 
 
    
-
-	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        Canvas.gameObject.SetActive(false);
         //Fire.SetActive(false);
         level = 1;
         foreach (GameObject item in Level2Barrels)
@@ -31,7 +31,7 @@ public class PlayerController : MonoBehaviour {
         }
 	}
 
-    IEnumerator Invincibility()
+    IEnumerator Invincibility() //Näkymättömyys, joka aktivoituu, kun pelaaja ottaa vahinkoa.
     {
         IsInvincible = true;
         gameObject.tag = "Untagged";
@@ -61,9 +61,8 @@ public class PlayerController : MonoBehaviour {
         gameObject.layer = 9;
     }
 	
-	// Update is called once per frame
 	void Update () {
-        if (Input.GetButton("Vertical"))
+        if (Input.GetButton("Vertical")) //Jos painaa ylös, menee ylös. Magic!
         {
            
             {
@@ -72,11 +71,11 @@ public class PlayerController : MonoBehaviour {
             }
         }
 
-        if (Input.GetButton("Horizontal")){
+        if (Input.GetButton("Horizontal")){ //Hallitsee kääntämistä. RotationSpeed hallitsee kääntymisnopeutta.
             rb2d.rotation = rb2d.rotation + rotationspeed * Input.GetAxis("Horizontal") * -1;
         }
 
-        if (Input.GetButtonUp("Vertical"))
+        if (Input.GetButtonUp("Vertical")) //Vanha toiminto, jossa raketille ilmestyi tulta kun liikkui.
         {
             //Fire.SetActive(false);
         }
@@ -85,7 +84,7 @@ public class PlayerController : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
-        if (collision.tag == "PowerUp")
+        if (collision.tag == "PowerUp") //Powerupin saanti.
         {
             Debug.Log("Got a powerup!");
             if (level == 1) //Level 2.
@@ -120,8 +119,8 @@ public class PlayerController : MonoBehaviour {
         {
             if (IsInvincible == false)
             {
-                
                 Debug.Log("Met an enemy");
+                StartCoroutine(Invincibility());
                 if (level == 3) //Laskeminen levelistä 3 leveliin 2.
                 {
                     level--;
@@ -139,18 +138,24 @@ public class PlayerController : MonoBehaviour {
                     {
                         item.SetActive(false);
                     }
+                    foreach (GameObject item in Level1Barrels)
+                    {
+                        item.SetActive(true);
+                    }
                     return;
                 }
 
 
-                if (level == 1)
+                if (level == 1) //Game Over!
                 {
                     ExplosionSprite.transform.position = gameObject.transform.position;
                     Destroy(gameObject);
                     ExplosionSprite.GetComponent<Animator>().Play("Explosion");
-
+                    Canvas.gameObject.SetActive(true);
+                    Canvas.gameObject.GetComponent<Animator>().SetTrigger("GameOver");
+                    
                 }
-            StartCoroutine(Invincibility());
+            
             }
          Debug.Log(level);
         }
