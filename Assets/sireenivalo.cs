eb332f10692ed95d9c196b247ff220368d6b846e
Unknown <jaronhh1@gmail.com>
2018-06-07 09:12:46 +0300

    sireenivalo

---
+++ Assets/sireenivalo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sireenivalo : MonoBehaviour
+{
+    Vector3 minunVektori; //minunVektori = new Vector3(0, 0, 0);
+    public float nopeus = 200f;
+
+    private void Start()
+    {
+        Debug.Log("hmmm");
+        minunVektori = new Vector3(0, 0, 1);
+    }
+    // Debug.Log("Jotaintekstiä");
+
+    void Update ()
+    {
+        transform.Rotate(minunVektori * Time.deltaTime * nopeus);
+	}
+}
