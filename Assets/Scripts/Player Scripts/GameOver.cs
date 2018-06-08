48ed5f714b2aed0a0e866bb4215b5afbb5b9ee05
Sasha Usoskin <sashausoskin@live.com>
2018-06-08 15:00:09 +0300

    GameoverUI + PlayerExplosion Instanstiate
    GameOver-scripti vetää nyt pelaajan räjähdyksen ja GameOverUI:n prefabeista jotta elämä olisi helpompaa tulevia Koodikärppiä™ varten.

--- Assets/Scripts/Player Scripts/GameOver.cs
+++ Assets/Scripts/Player Scripts/GameOver.cs
@@ -6,21 +6,31 @@ public class GameOver : MonoBehaviour {
 
     public Canvas GameOverCanvas;
     public GameObject ExplosionSprite;
+    public List<Canvas> CanvasesToDestroyWhenGameOver = new List<Canvas>();
 
 	// Use this for initialization
 	void Start () {
-        GameOverCanvas.gameObject.GetComponent<Animator>().playbackTime = 0;
-        GameOverCanvas.gameObject.GetComponent<Animator>().speed = 0;
-        GameOverCanvas.gameObject.SetActive(false);
+        foreach (Canvas item in CanvasesToDestroyWhenGameOver)
+        {
+            item.gameObject.SetActive(true);
+        }
+        //GameOverCanvas.gameObject.GetComponent<Animator>().playbackTime = 0;
+        //GameOverCanvas.gameObject.GetComponent<Animator>().speed = 0;
+        //GameOverCanvas.gameObject.SetActive(false);
     }
 	
 	// Update is called once per frame
 	public void GameOverAnimation()
     {
-        GameOverCanvas.gameObject.SetActive(true);
-        ExplosionSprite.transform.position = gameObject.transform.position;
+
+        //GameOverCanvas.gameObject.SetActive(true);
+        Instantiate(GameOverCanvas, gameObject.transform.position, Quaternion.identity);
+        Instantiate(ExplosionSprite, gameObject.transform.position, Quaternion.identity);
         gameObject.SetActive(false);
-        ExplosionSprite.GetComponent<Animator>().Play("Explosion");
-        GameOverCanvas.gameObject.GetComponent<Animator>().speed = 1;
+        foreach (Canvas item in CanvasesToDestroyWhenGameOver)
+        {
+            item.gameObject.SetActive(false);
+        }
+        
     }
 }
