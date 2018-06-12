c8ce7e8e730fa8641d32d65ddd35e0ac5dbe7540
Unknown <sashausoskin@live.com>
2018-06-12 12:29:03 +0300

    OGRaketti-bossi
    Lisätty OGRaketti-bossi, jolla on paljon healthia ja joka ampuu jatkuvasti ohjuksia. Tällä bossilla on myös musiikkia. Olen yrittänyt tehdä scripteistä niin modulaarisia kuin mahdollista, jotta niitä voisi käyttää myös muissa bosseissa.
    
    TODO:
    -Mahdollisesti parempaa arttia bossille. Tämä on vain ajatus.
    -Bossille pitäisi laittaa musiikkia, joka ei riko tekijänoikeuslakia.
    -Nykyinen musiikki pitäisi leikata.
    -Pitäisi vielä muokkailla ohjuksien arvoja.

---
+++ Assets/Scripts/Bosses/BossActivate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OGRocketBossActivate : MonoBehaviour {
+    public GameObject BossRestriction;
+    private GameObject Player;
+    private GameObject Camera;
+    public Transform BossLocation;
+    public GameObject Boss;
+    public bool BossHasStarted;
+    public float CameraSizeInBossBattle;
+    public AudioClip BossMusic;
+
+    private void Start()
+    {
+        BossRestriction.SetActive(false);
+    }
+
+    IEnumerator ActivateBoss()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Player.GetComponent<PlayerMovement>().enabled = false;
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+        Camera.GetComponent<CameraController>().enabled = false;
+        yield return new WaitForSeconds(2);
+        Camera.transform.position = new Vector3(BossRestriction.transform.position.x, BossRestriction.transform.position.y, -1);
+        yield return new WaitForSeconds(1);
+        BossRestriction.SetActive(true);
+        yield return new WaitForSeconds(1);
+        Camera.GetComponent<Camera>().orthographicSize = 20;
+        Camera.transform.position = new Vector3(BossLocation.transform.position.x, BossLocation.transform.position.y, -1);
+        yield return new WaitForSeconds(1);
+        Instantiate(Boss, BossLocation.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        Camera.GetComponent<CameraController>().enabled = true;
+        Camera.GetComponent<Camera>().orthographicSize = CameraSizeInBossBattle;
+        Player.GetComponent<PlayerMovement>().enabled = true;
+        Player.GetComponent<AudioSource>().clip = BossMusic;
+        Player.GetComponent<AudioSource>().Play();
+        BossHasStarted = true;
+        
+
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(ActivateBoss());
+        }
+    }
+}
---
+++ Assets/Scripts/Bosses/RocketBossStart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBossStart : MonoBehaviour {
+
+    private bool HasBeenActivated;
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (HasBeenActivated == false)
+        {
+            if (GameObject.FindGameObjectWithTag("BossActivation").GetComponent<OGRocketBossActivate>().BossHasStarted == true)
+            {
+                foreach (Transform child in transform)
+                {
+                    GameObject.FindGameObjectWithTag("BossActivation").SetActive(false);
+                    foreach (Transform childitem in transform)
+                    {
+                        childitem.gameObject.SetActive(true);
+                    }
+                    HasBeenActivated = true;
+                    break;
+                }
+            }
+        }
+	}
+}
