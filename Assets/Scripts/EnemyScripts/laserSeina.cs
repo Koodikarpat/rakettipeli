0e40fb49d78572b4c0c6e710f18c0d428d10371e
Unknown <jaronhh1@gmail.com>
2018-06-12 09:46:19 +0300

    Kolikko, kauppa, päälle ja pois menevä laaseri, piikkimiina

---
+++ Assets/Scripts/EnemyScripts/laserSeina.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserSeina : MonoBehaviour
+{
+    float aika;
+    public float paallaoloAika = 5f;
+    public float kiinnimenoAika = 10f;
+
+    private void Update()
+    {
+        aika = aika + Time.deltaTime;
+        if (aika > paallaoloAika)
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+        if (aika > kiinnimenoAika)
+        {
+            GetComponent<Renderer>().enabled = true;
+            aika = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/piikitLiikkumaan.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class piikitLiikkumaan : MonoBehaviour
+{
+    public GameObject[] piikit;
+    Vector3 pyoriminen;
+    public float pyorimisNopeus = 10f;
+
+    private void Start()
+    {
+        pyoriminen = new Vector3(0, 0, 1);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(pyoriminen * Time.deltaTime * pyorimisNopeus);
+    }
+
+    void OnTriggerEnter2D(Collider2D mihinTormattiin)
+    {
+        for (int i = 0; i < piikit.Length; i++)
+        {
+            piikit[i].GetComponent<piikki>().pitaakoLiikkua = true;
+            piikit[i].transform.parent = null;
+        }
+    }
+}
