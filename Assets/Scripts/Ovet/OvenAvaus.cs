53699b7828794db0bc36ef1b6119af5c5228cc84
Unknown <matleena.oikarinen@gmail.com>
2018-06-13 09:45:21 +0300

    OVIA LEVEL2 POHJA
    ihan hyvä

--- Assets/Scripts/Ovet/OvenAvaus.cs
+++ Assets/Scripts/Ovet/OvenAvaus.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class OvenAvaus : MonoBehaviour {
 
     public bool onkoPelaajallaAvain;
+    Animator m_Animator;
+    private GameObject avainkortti;
+    public bool saapuuOvelle;
+
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
 
     void OnTriggerEnter2D(Collider2D mihinTormattiin)
 
     { 
-    if (mihinTormattiin.GetComponent<PelaajanAvaimet>().onkoPelaajallaAvain)
+    if (mihinTormattiin.gameObject.tag.Equals("Player") && mihinTormattiin.GetComponent<PelaajanAvaimet>().onkoPelaajallaAvain)
         {
             Debug.Log ("Ovi avautuu");
-            Destroy(gameObject);
+        
+            m_Animator.SetTrigger("AvaaOvi2");
+
+            saapuuOvelle = true;
         }
-    else
+        else
         {
             Debug.Log ("Ovi ei avaudu");
         }
