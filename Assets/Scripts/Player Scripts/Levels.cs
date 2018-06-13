2405778743cec8cc2537beab0c6d839ddf108ad2
Unknown <sashausoskin@live.com>
2018-06-13 14:58:40 +0300

    Level 5
    Pelaaja voi nyt nousta leveliin 5 asti. Lisäksi commitattu loppuun edellinen committi.

--- Assets/Scripts/Player Scripts/Levels.cs
+++ Assets/Scripts/Player Scripts/Levels.cs
@@ -8,11 +8,16 @@ public class Levels : MonoBehaviour
     public int level;
     public GameObject[] Level1Barrels; //Barrelit, joita käytetään levelissä 1.
     public GameObject[] Level2Barrels; //Barrelit, joita käytetään levelissä 2.
+    public GameObject[] Level4Barrels; //Barrelit, joita käytetään levelissä 4.
 
     private void Start()
     {
         level = 1;
         foreach (GameObject item in Level2Barrels)
+        {
+            item.SetActive(false);
+        }
+        foreach (GameObject item in Level4Barrels)
         {
             item.SetActive(false);
         }
@@ -42,7 +47,32 @@ public class Levels : MonoBehaviour
             {
                 item.SetActive(true);
             }
+        return;
+        }
+        if (level == 3) //Level 4. Käytetään levelin 2 ja levelin 4 tykkejä.
+        {
+            level++;
+            foreach (GameObject item in Level4Barrels)
+            {
+                item.SetActive(true);
+            }
+            foreach (GameObject item in Level1Barrels)
+            {
+                item.SetActive(false);
+            }
+            return;
+        }
+            
+        if (level == 4) //Level 5. Käytetään kaikkia tykkejä yhtä aikaa.
+        {
+            level++;
+            foreach (GameObject item in Level1Barrels)
+            {
+                    item.SetActive(true);
+            }
+            return;
         }
     }
 }
+
 
