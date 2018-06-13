db57955f5e9ff046fd744cb8f236854583bcec79
Unknown <samuel.ylimaki@gmail.com>
2018-06-13 12:04:20 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Background/TaustanLiike.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaustanLiike : MonoBehaviour {
+
+    
+    public float liikeMäärä;
+
+    private Vector3 camStart;
+    private Transform camMove;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        camMove = Camera.main.transform;
+        
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        camStart = new Vector3 (camMove.position.x, camMove.position.y, 0);
+        transform.position = new Vector3(camMove.position.x, camMove.position.y, 9) - camStart * liikeMäärä ;
+	}
+}
