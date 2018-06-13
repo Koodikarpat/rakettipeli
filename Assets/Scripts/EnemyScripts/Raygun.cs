13d38eeff2417968e70c63269376e84a865ccfea
Unknown <samuel.ylimaki@gmail.com>
2018-06-13 10:05:08 +0300

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/EnemyScripts/Raygun.cs
+++ Assets/Scripts/EnemyScripts/Raygun.cs
@@ -4,43 +4,85 @@ using UnityEngine;
 
 public class Raygun : MonoBehaviour
 {
-    bool active = true;
-    float aika = 0;
-    public float reloadTime = 2;
+    public bool active = true;
+    float aika = 0f;
+    float aika1 = 0f;
+    public float charge = 2;
     public GameObject ammus;
     public GameObject barrelEnd;
-    Vector2 suunta;
+    public GameObject tähtäin;
+    
+    
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
+    {
+        tähtäin.SetActive(false);
+        ammus.SetActive(false);
+        active = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) //tunnistaa, jos pelaaja on tykin rangella.
     {
         if (collision.tag == "Player")
         {
-           
-           active = false;
-
+                           
+                active = false;
+                aika = 0;
+            
         }
         
 
     }
     private void Update()
     {
+        Debug.Log(aika);
+        RaycastHit2D[] hits;
+        Debug.DrawRay(barrelEnd.transform.position, transform.up);
+        hits = Physics2D.RaycastAll(barrelEnd.transform.position, transform.up, 100f); // Tunnistaa kaikki objektit  100 yksikön päähän tykin edessä.
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.transform.tag == "LevelRestriction")
+            {
+                tähtäin.transform.localScale = new Vector3(1, hit.distance, 1);
+                ammus.transform.localScale = new Vector3(1, hit.distance, 1);//muuttaa tähtäimen ja ammuksen pituuden oikeaksi.
+
+            }
+        }
         if (active == false )
         {
-            
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up) ;
-            Debug.Log(hit);
-            if (aika == 0)
-            {
-                Instantiate(ammus, barrelEnd.transform.position, barrelEnd.transform.rotation);
-            }
             GetComponent<BoxCollider2D>().enabled = false;
             aika = aika + Time.deltaTime;
             
-            if (aika >= reloadTime)
+            
+            
+            if (aika > 1)
             {
-                GetComponent<BoxCollider2D>().enabled = true;
-                aika = 0;
+                tähtäin.SetActive(true);
             }
+            
+            
+
+
+
+            if (aika >= charge)
+            {
+                ammus.SetActive(true);
+                tähtäin.SetActive(false);
+
+               
+                
+                
+            }
+            if (aika >= charge + 0.5f)
+            {
+                ammus.SetActive(false);
+                aika = 0;
+                active = true;
+
+            }
+
+            
         }
     }
 }
---
+++ Assets/Scripts/EnemyScripts/RaygunRotator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaygunRotator : MonoBehaviour
+{
+
+    private GameObject targetPosition;
+    private Transform Position;
+    Vector2 suunta;
+    float kulma;
+    public float nopeus = 1;
+    float aika = 0f;
+    Raygun rayScript; // raygun skripti
+
+    private void Start()
+    {
+        rayScript = GetComponent<Raygun>();
+        targetPosition = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        
+        if (rayScript.active)
+        {
+
+                Position = targetPosition.transform;
+
+                suunta = targetPosition.transform.position - transform.position;
+
+                kulma = Mathf.Atan2(suunta.y, suunta.x) * Mathf.Rad2Deg - 90;
+
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, kulma), nopeus);
+            
+        }
+
+    }
+
+}
