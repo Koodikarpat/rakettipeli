f42fd6f20dd031cb5162786103c9fbf506124459
Unknown <matleena.oikarinen@gmail.com>
2018-06-14 09:59:10 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/EnemyScripts/LaserAvainTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserAvainTrigger : MonoBehaviour {
+
+    public GameObject ansat;
+    public Color altColor;
+    public Renderer rend;
+
+    void Start ()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D mihinTormattiin)
+    {
+         if(mihinTormattiin.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Nappia painettu");
+            ansat.SetActive(false);
+            altColor.g = 10f;
+            rend.material.color = altColor;
+        }
+    }
+}
