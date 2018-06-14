03d9db0d9231c39fd40dc4cb4c516ee0f62af363
Unknown <anders.nikula@gmail.com>
2018-06-14 12:11:42 +0300

    Kolikon saanti teksti
    kolikon saanti teksti ja healthPickUp script

---
+++ Assets/Scripts/pickups/HealthPickUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : MonoBehaviour {
+
+    PlayerHealth PlayerHealthScript;
+
+    private void Start()
+    {
+        PlayerHealthScript = GameObject.Find("Raketti").GetComponent<PlayerHealth>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") //Pelaajalla on vielä kolmas collider lapsena, jotta pickupit toimisivat. Jos käyttäisi pelaajan collidereita, pelaaja saisi aina 2 leveliä yhdestä pickupista.
+        {
+            PlayerHealthScript.playerHealth++;
+            Destroy(gameObject);
+        }
+    }
+}
