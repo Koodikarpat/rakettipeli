e8aaae1de0682d0831357ffe16db7540816bda4b
Unknown <anders.nikula@gmail.com>
2018-06-14 11:09:33 +0300

    Häkki valmis

---
+++ Assets/Scripts/Ovet/HäkkiovenAukaisija.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HäkkiovenAukaisija : MonoBehaviour {
+
+    Animator m_Animator;
+
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            m_Animator.SetTrigger("AvaaOvi");
+        }
+    }
+}
